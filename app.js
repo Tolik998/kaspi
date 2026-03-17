b0b764bc29613bc8a89bccc0cfa20a5b885207c7
Tolik998 <tolegentilegen3@gmail.com>
2026-03-17 09:10:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ function govScreen() {
     <div style="position:relative;width:100%;background:#f5f5f7;">
       <button data-nav="home" style="position:absolute;top:1%;left:2%;width:10%;height:4%;background:transparent;border:none;cursor:pointer;z-index:10;"></button>
       <img src="data:image/png;base64,${b64}" style="width:100%;display:block;" />
-      <button data-nav="idcard" style="position:absolute;top:36%;left:2%;width:46%;height:16%;background:transparent;border:none;cursor:pointer;"></button>
+      <button data-nav="idcard" style="position:absolute;top:28%;left:2%;width:46%;height:16%;background:transparent;border:none;cursor:pointer;"></button>
     </div>`;
 }
 
